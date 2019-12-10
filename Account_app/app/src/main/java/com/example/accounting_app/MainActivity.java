2a0ec1fdcb6856d3672aae81128321d970b8851e
HoWanJu <annie1999706@gmail.com>
2019-12-10 16:00:15 +0800

    類別管理介面

--- Account_app/app/src/main/java/com/example/accounting_app/MainActivity.java
+++ Account_app/app/src/main/java/com/example/accounting_app/MainActivity.java
@@ -12,19 +12,10 @@ import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = (Button) findViewById(R.id.BUTTON);
-        btn.setOnClickListener(example);
+
     }
-    private View.OnClickListener example = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this, Role_choose.class);
-            startActivity(intent);
-        }
-    };
 }
