a0265dccfbdb88c6cbb1b1a0d349455d499edf63
jinghan <7jesstc1999@gmail.com>
2019-12-24 22:33:35 +0800

    預算可hint原資料庫&update新預算

--- Account_app/app/src/main/java/com/example/accounting_app/Budget.java
+++ Account_app/app/src/main/java/com/example/accounting_app/Budget.java
@@ -11,8 +11,40 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
 
 public class Budget extends AppCompatActivity {
+    public FirebaseAuth mAuth;
+    public Integer nowBudge;
+    public Integer nowMonth;
+    public Integer nowYear;
+    private String uid;
+    private FirebaseUser user;
+    private Integer budget;
+
+    Calendar cal = Calendar.getInstance();
+//    cal.setTime(new Date());
+
     ImageButton backBTN;
     Button confirmBTN;
     EditText inputEdT;
@@ -22,6 +54,32 @@ public class Budget extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget);
+        mAuth = FirebaseAuth.getInstance();
+
+        inputEdT = findViewById(R.id.inputET);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        uid = user.getUid();
+
+        nowYear = cal.get(Calendar.YEAR);
+        nowMonth =  cal.get(Calendar.MONTH) +1;
+
+        //wirte to database
+        //連接資料庫
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = firebaseDatabase.getReference("accounting_record/" + uid + "/" + nowYear.toString() + "/" + nowMonth.toString());
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                accounting_month accounting_month = dataSnapshot.getValue(accounting_month.class);
+                budget = accounting_month.getM_budget();
+                inputEdT.setHint(budget.toString());
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
         goBack();
         confirm();
     }
@@ -49,6 +107,7 @@ public class Budget extends AppCompatActivity {
                     new AlertDialog.Builder(Budget.this)
                             .setTitle("請輸入金額")
                             .setPositiveButton("確定", null).create().show();
+                    Toast.makeText(Budget.this, nowYear, Toast.LENGTH_SHORT).show();
                 }
                 else{
                     budgetAlertDialog();
@@ -58,12 +117,29 @@ public class Budget extends AppCompatActivity {
     }
 
     private void budgetAlertDialog(){
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        inputEdT = findViewById(R.id.inputET);
+//
+//        user = FirebaseAuth.getInstance().getCurrentUser();
+//        uid = user.getUid();
+//
+//        nowYear = cal.get(Calendar.YEAR);
+//        nowMonth =  cal.get(Calendar.MONTH) +1;
+
         new AlertDialog.Builder(Budget.this)
                 .setTitle("確定要輸入預算嗎")
                 .setPositiveButton("確定", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         confirmStr = "已輸入金額"+inputEdT.getText().toString()+"元";
+
+                        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                        DatabaseReference myRef_update = firebaseDatabase.getReference("accounting_record");
+                        Map<String, Object> childUpdates = new HashMap<>();
+                        childUpdates.put("m_budget",Integer.parseInt(inputEdT.getText().toString()));
+                        myRef_update.child(uid).child(nowYear.toString()).child(nowMonth.toString()).updateChildren(childUpdates);
+
                         Toast.makeText(Budget.this,confirmStr, Toast.LENGTH_LONG).show();
                         Intent intent = new Intent(Budget.this, Function.class);
                         startActivity(intent);
